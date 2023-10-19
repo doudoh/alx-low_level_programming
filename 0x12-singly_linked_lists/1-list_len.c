eba7f5b11e7bcf5098eacc9710c36a11e872d466
doudoh <houdabd10@gmail.com>
2023-10-19 12:02:46 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-/**
-* list_len - determines length of linked list
-* @h: pointer of the first node
-*
-* Return: size of list
-*/
-size_t print_list(const list_t *h)
-{
-        size_t i = 0;
 
-        while (h)
-        {
-                h = h->next;
-                i++;
-        }
-        return (i);
+
+/**
+* list_len - check the code for ALX School students.
+* @h: name of the list
+* Return: the number of nodes.
+*/
+size_t list_len(const list_t *h)
+{
+   int count = 0;
+
+
+   while (h)
+   {
+       count++;
+       h = h->next;
+   }
+   return (count);
 }
